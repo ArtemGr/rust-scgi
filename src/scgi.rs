233c6235b8598d441d53603fa151b0b3378b0fea
ArtemGr <artemciy@gmail.com>
2014-12-23 20:01:13 +0300

    Upgraded to work with Rust nightly.

--- src/scgi.rs
+++ src/scgi.rs
@@ -1,6 +1,7 @@
 //! SCGI parser.
 //!
 //! This is partly a port of my Java parser: https://gist.github.com/ArtemGr/38425.
+// [build] cd .. && cargo test
 
 #![feature(slicing_syntax)]
 #![feature(default_type_params)]
@@ -13,11 +14,12 @@ use std::collections::HashMap;
 use std::error::FromError;
 use std::io::{BufferedStream, IoError};
 use std::io::net::tcp::{TcpStream};
-use std::str::from_utf8;
+use std::str::{from_utf8, Utf8Error};
 
 #[cfg(test)] use std::io::{Listener, Acceptor};
 #[cfg(test)] use std::io::net::tcp::{TcpListener};
 #[cfg(test)] use std::io::timer::sleep;
+#[cfg(test)] use std::thread::Thread;
 #[cfg(test)] use std::time::duration::Duration;
 
 use ScgiError::*;
@@ -25,8 +27,10 @@ use ScgiError::*;
 /// SCGI parsing errors.
 #[deriving(Show)]
 pub enum ScgiError {
-  /// Length can't be UTF-8 decoded to a string or an integer.
+  /// Length can't be decoded to an integer.
   BadLength,
+  /// The length or the headers are not in UTF-8.
+  Utf8 (Utf8Error),
   /// Netstring sanity checks fail.
   WrongLength (String),
   /// Error parsing the zero-terminated HTTP headers.
@@ -34,9 +38,8 @@ pub enum ScgiError {
   /// IoError, like when connection closed prematurely.
   IO (IoError)
 }
-impl FromError<IoError> for ScgiError {
-  fn from_error (io_error: IoError) -> ScgiError {IO (io_error)}
-}
+impl FromError<IoError> for ScgiError {fn from_error (io_error: IoError) -> ScgiError {IO (io_error)}}
+impl FromError<Utf8Error> for ScgiError {fn from_error (utf8_error: Utf8Error) -> ScgiError {Utf8 (utf8_error)}}
 
 /// Read the headers from the stream.
 ///
@@ -53,14 +56,14 @@ pub fn read_headers (tcp_stream: TcpStream) -> Result<(Vec<u8>, BufferedStream<T
     if ch >= '0' && ch <= '9' {
       length_string[length_string_len] = ch as u8; length_string_len += 1;
     } else if ch == ':' {
-      let length_str = try! (from_utf8 (length_string[..length_string_len]) .ok_or (BadLength));
-      let length: uint = try! (from_str (length_str) .ok_or (BadLength));
+      let length_str = try! (from_utf8 (length_string[..length_string_len]));
+      let length: uint = try! (length_str.parse().ok_or (BadLength));
       let headers_buf = try! (stream.read_exact (length));
       if try! (stream.read_char()) != ',' {return Err (WrongLength (length_str.to_string()))}
       raw_headers = headers_buf; break;
     } else {
       length_string[length_string_len] = ch as u8; length_string_len += 1;
-      return Err (WrongLength (try! (from_utf8 (length_string[..length_string_len]) .ok_or (BadLength)).to_string()));
+      return Err (WrongLength (try! (from_utf8 (length_string[..length_string_len])).to_string()));
     }
   };
   Ok ((raw_headers, stream))
@@ -71,10 +74,10 @@ pub fn parse<'h> (raw_headers: &'h Vec<u8>, header: |&'h str,&'h str|) -> Result
   let mut pos = 0u;
   while pos < raw_headers.len() {
     let zero1 = try! (raw_headers[pos..].iter().position (|&ch|ch == 0) .ok_or (WrongHeaders));
-    let header_name = try! (from_utf8 (raw_headers[pos .. pos + zero1]) .ok_or (WrongHeaders));
+    let header_name = try! (from_utf8 (raw_headers[pos .. pos + zero1]));
     pos = pos + zero1 + 1;
     let zero2 = try! (raw_headers[pos..].iter().position (|&ch|ch == 0) .ok_or (WrongHeaders));
-    let header_value = try! (from_utf8 (raw_headers[pos .. pos + zero2]) .ok_or (WrongHeaders));
+    let header_value = try! (from_utf8 (raw_headers[pos .. pos + zero2]));
     header (header_name, header_value);
     pos = pos + zero2 + 1;
   }
@@ -97,13 +100,13 @@ pub fn str_map<'h> (raw_headers: &'h Vec<u8>) -> Result<HashMap<&'h str, &'h str
 
 #[test] fn test_scgi() {
   let port = 13123;
-  spawn (proc() {
+  Thread::spawn (move|| {
     sleep (Duration::milliseconds (10));
     let mut stream = TcpStream::connect (("127.0.0.1", port));
     stream.write (b"70:CONTENT_LENGTH\x0056\x00SCGI\x001\x00REQUEST_METHOD\x00POST\x00REQUEST_URI\x00/deepthought\x00,") .unwrap();
     stream.write (b"What is the answer to life, the Universe and everything?") .unwrap();
     assert_eq! (stream.read_to_string().unwrap()[], "Status: 200 OK\r\nContent-Type: text/plain\r\n\r\n42");
-  });
+  }) .detach();
   let mut acceptor = TcpListener::bind (("127.0.0.1", port)) .unwrap().listen().unwrap();
   acceptor.set_timeout (Some (100));
   let stream = acceptor.incoming().next().unwrap();
