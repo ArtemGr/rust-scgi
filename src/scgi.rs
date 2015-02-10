ba1b1c40623c245bfdf52fcc80a3d4873dc23d11
ArtemGr <artemciy@gmail.com>
2015-02-10 22:22:29 +0300

    Upgrade.

--- src/scgi.rs
+++ src/scgi.rs
@@ -3,28 +3,25 @@
 //! This is partly a port of my Java parser: https://gist.github.com/ArtemGr/38425.
 // [build] cd .. && cargo test
 
-#![feature(slicing_syntax)]
-#![allow(unstable)]
-
-//extern crate rustc;
+#![feature(io,core,std_misc)]
 
 //use rustc::util::nodemap::FnvHasher;  // http://www.reddit.com/r/rust/comments/2l4kxf/std_hashmap_is_slow/
 use std::collections::HashMap;
 use std::error::FromError;
-use std::io::{BufferedStream, IoError};
-use std::io::net::tcp::{TcpStream};
+use std::old_io::{BufferedStream, IoError};
+use std::old_io::net::tcp::{TcpStream};
 use std::str::{from_utf8, Utf8Error};
 
-#[cfg(test)] use std::io::{Listener, Acceptor};
-#[cfg(test)] use std::io::net::tcp::{TcpListener};
-#[cfg(test)] use std::io::timer::sleep;
+#[cfg(test)] use std::old_io::{Listener, Acceptor};
+#[cfg(test)] use std::old_io::net::tcp::{TcpListener};
+#[cfg(test)] use std::old_io::timer::sleep;
 #[cfg(test)] use std::thread::Thread;
 #[cfg(test)] use std::time::duration::Duration;
 
 use ScgiError::*;
 
 /// SCGI parsing errors.
-#[derive(Show)]
+#[derive(Debug)]
 pub enum ScgiError {
   /// Length can't be decoded to an integer.
   BadLength,
@@ -55,14 +52,14 @@ pub fn read_headers (tcp_stream: TcpStream) -> Result<(Vec<u8>, BufferedStream<T
     if ch >= '0' && ch <= '9' {
       length_string[length_string_len] = ch as u8; length_string_len += 1;
     } else if ch == ':' {
-      let length_str = try! (from_utf8 (length_string.slice (0, length_string_len)));
-      let length: usize = try! (length_str.parse().ok_or (BadLength));
+      let length_str = try! (from_utf8 (&length_string[0 .. length_string_len]));
+      let length: usize = try! (length_str.parse().map_err (|_| BadLength));
       let headers_buf = try! (stream.read_exact (length));
       if try! (stream.read_char()) != ',' {return Err (WrongLength (length_str.to_string()))}
       raw_headers = headers_buf; break;
     } else {
       length_string[length_string_len] = ch as u8; length_string_len += 1;
-      return Err (WrongLength (try! (from_utf8 (length_string.slice (0, length_string_len))).to_string()));
+      return Err (WrongLength (try! (from_utf8 (&length_string[0 .. length_string_len])).to_string()));
     }
   };
   Ok ((raw_headers, stream))
@@ -73,10 +70,10 @@ pub fn parse<'h,H> (raw_headers: &'h Vec<u8>, mut header: H) -> Result<(), ScgiE
   let mut pos = 0us;
   while pos < raw_headers.len() {
     let zero1 = try! (raw_headers[pos..].iter().position (|&ch|ch == 0) .ok_or (WrongHeaders));
-    let header_name = try! (from_utf8 (raw_headers.slice (pos, pos + zero1)));
+    let header_name = try! (from_utf8 (&raw_headers[pos .. pos + zero1]));
     pos = pos + zero1 + 1;
     let zero2 = try! (raw_headers[pos..].iter().position (|&ch|ch == 0) .ok_or (WrongHeaders));
-    let header_value = try! (from_utf8 (raw_headers.slice (pos, pos + zero2)));
+    let header_value = try! (from_utf8 (&raw_headers[pos .. pos + zero2]));
     header (header_name, header_value);
     pos = pos + zero2 + 1;
   }
@@ -86,14 +83,14 @@ pub fn parse<'h,H> (raw_headers: &'h Vec<u8>, mut header: H) -> Result<(), ScgiE
 /// Parse the headers and pack them as strings into a map.
 pub fn string_map (raw_headers: &Vec<u8>) -> Result<HashMap<String, String>, ScgiError> {
   let mut headers_map = std::collections::HashMap::with_capacity (48);
-  try! (parse (raw_headers, |&mut: name,value| {headers_map.insert (name.to_string(), value.to_string());}));
+  try! (parse (raw_headers, |name,value| {headers_map.insert (name.to_string(), value.to_string());}));
   Ok (headers_map)
 }
 
 /// Parse the headers and pack them as slices into a map.
 pub fn str_map<'h> (raw_headers: &'h Vec<u8>) -> Result<HashMap<&'h str, &'h str>, ScgiError> {
   let mut headers_map = std::collections::HashMap::with_capacity (48);
-  try! (parse (raw_headers, |&mut: name,value| {headers_map.insert (name, value);}));
+  try! (parse (raw_headers, |name,value| {headers_map.insert (name, value);}));
   Ok (headers_map)
 }
 
@@ -102,8 +99,8 @@ pub fn str_map<'h> (raw_headers: &'h Vec<u8>) -> Result<HashMap<&'h str, &'h str
   Thread::spawn (move|| {
     sleep (Duration::milliseconds (10));
     let mut stream = TcpStream::connect (("127.0.0.1", port));
-    stream.write (b"70:CONTENT_LENGTH\x0056\x00SCGI\x001\x00REQUEST_METHOD\x00POST\x00REQUEST_URI\x00/deepthought\x00,") .unwrap();
-    stream.write (b"What is the answer to life, the Universe and everything?") .unwrap();
+    stream.write_all (b"70:CONTENT_LENGTH\x0056\x00SCGI\x001\x00REQUEST_METHOD\x00POST\x00REQUEST_URI\x00/deepthought\x00,") .unwrap();
+    stream.write_all (b"What is the answer to life, the Universe and everything?") .unwrap();
     assert_eq! (stream.read_to_string().unwrap().as_slice(), "Status: 200 OK\r\nContent-Type: text/plain\r\n\r\n42");
   });
   let mut acceptor = TcpListener::bind (("127.0.0.1", port)) .unwrap().listen().unwrap();
@@ -115,7 +112,7 @@ pub fn str_map<'h> (raw_headers: &'h Vec<u8>) -> Result<HashMap<&'h str, &'h str
       let (raw_headers, mut stream) = read_headers (tcp_stream) .unwrap();
       assert_eq! (str_map (&raw_headers) .unwrap() ["REQUEST_URI"], "/deepthought");
       assert_eq! (string_map (&raw_headers) .unwrap() ["REQUEST_URI".to_string()] .as_slice(), "/deepthought");
-      stream.write (b"Status: 200 OK\r\nContent-Type: text/plain\r\n\r\n42") .unwrap();
+      stream.write_all (b"Status: 200 OK\r\nContent-Type: text/plain\r\n\r\n42") .unwrap();
     }
   }
 }
